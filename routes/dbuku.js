6763591fe71a30a9efc2ed8369f236b6d129166d
AlamRidha <alamrridha@gmail.com>
2025-02-26 11:23:09 +0700

    clean code

--- routes/dbuku.js
+++ routes/dbuku.js
@@ -8,82 +8,121 @@ const v = new Validator();
 
 // get all data
 router.get("/", async (req, res) => {
-  const dataBuku = await Buku.findAll();
-  return res.json(dataBuku);
+  try {
+    const dataBuku = await Buku.findAll();
+
+    if (dataBuku.length === 0) {
+      return res.status(404).json({ status: "success", msg: "Data Empty" });
+    }
+
+    return res
+      .status(200)
+      .json({ status: "success", msg: "Data Found", data: dataBuku });
+  } catch (error) {
+    return res.status(500).json({ status: "error", msg: error.message });
+  }
 });
 
 // get data by id
 router.get("/:id", async (req, res) => {
-  const id = req.params.id;
-  const dataBuku = await Buku.findByPk(id);
+  try {
+    const id = req.params.id;
+    const dataBuku = await Buku.findByPk(id);
 
-  if (!dataBuku) {
-    return res.status(404).json({ msg: "Data buku tidak ditemukan" });
+    if (!dataBuku) {
+      return res.status(404).json({ status: "error", msg: "Data Not Found" });
+    }
+
+    return res
+      .status(200)
+      .json({ status: "success", msg: "Data Found", sdata: dataBuku });
+  } catch (error) {
+    return res.status(500).json({ status: "error", msg: error.message });
   }
-
-  return res.json(dataBuku);
 });
 
 // create data buku
 router.post("/", async (req, res) => {
-  const schema = {
-    namaBuku: "string",
-    linkBuku: "string",
-  };
+  try {
+    const schema = {
+      namaBuku: "string",
+      linkBuku: "string",
+    };
 
-  const validate = v.validate(req.body, schema);
-  // cek validasi
-  if (validate.length) {
-    return res.status(400).json(validate);
+    const validate = v.validate(req.body, schema);
+    // cek validasi
+    if (validate.length) {
+      return res.status(400).json({ status: "error", msg: validate });
+    }
+
+    // res.send("ok");
+    const buku = await Buku.create(req.body);
+
+    res.status(200).json({
+      status: "success",
+      msg: "Buku Successfully Created",
+      data: buku,
+    });
+  } catch (error) {
+    res.status(500).json({ status: "error", msg: error.message });
   }
-
-  // res.send("ok");
-  const buku = await Buku.create(req.body);
-
-  res.json(buku);
 });
 
 // update data
 router.put("/:id", async (req, res) => {
-  const id = req.params.id;
+  try {
+    const id = req.params.id;
 
-  let buku = await Buku.findByPk(id);
+    let buku = await Buku.findByPk(id);
 
-  // cek buku di db
-  if (!buku) {
-    return res.status(400).json({ msg: "Buku tidak ditemukan" });
+    if (!buku) {
+      return res
+        .status(400)
+        .json({ status: "error", msg: "Buku tidak ditemukan" });
+    }
+
+    const schema = {
+      namaBuku: "string|optional",
+      linkBuku: "string|optional",
+    };
+
+    const validate = v.validate(req.body, schema);
+    // cek validasi
+    if (validate.length) {
+      return res.status(400).json({ status: "error", msg: validate });
+    }
+
+    buku = await buku.update(req.body);
+
+    res.status(200).json({
+      status: "success",
+      msg: "Buku Successfully Updated",
+      data: buku,
+    });
+  } catch (error) {
+    res.status(500).json({ status: "error", msg: error.message });
   }
-
-  const schema = {
-    namaBuku: "string|optional",
-    linkBuku: "string|optional",
-  };
-
-  const validate = v.validate(req.body, schema);
-  // cek validasi
-  if (validate.length) {
-    return res.status(400).json(validate);
-  }
-
-  buku = await buku.update(req.body);
-
-  res.json(buku);
 });
 
 // delete data
 router.delete("/:id", async (req, res) => {
-  const id = req.params.id;
+  try {
+    const id = req.params.id;
 
-  const buku = await Buku.findByPk(id);
+    const buku = await Buku.findByPk(id);
 
-  if (!buku) {
-    return res.status(404).json({ msg: "Data buku tidak ditemukan" });
+    if (!buku) {
+      return res.status(404).json({ status: "error", msg: "Data Not Found" });
+    }
+
+    await buku.destroy();
+
+    res
+      .status(200)
+      .json({ status: "success", msg: "Buku Successfully Deleted" });
+  } catch (error) {
+    res.status(500).json({ status: "error", msg: error.message });
   }
-
-  await buku.destroy();
-
-  res.json({ msg: "Data buku berhasil dihapus" });
 });
 
 module.exports = router;
-// done
